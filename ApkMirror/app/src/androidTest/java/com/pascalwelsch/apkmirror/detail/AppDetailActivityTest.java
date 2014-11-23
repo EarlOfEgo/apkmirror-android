cd293e7530b3434981da3ba15775b03190ac353d
Pascal Welsch <pascal.welsch@gmail.com>
2014-11-24 00:56:05 +0100

    prepared design. added parallax and fading toolbar

--- ApkMirror/app/src/androidTest/java/com/pascalwelsch/apkmirror/detail/AppDetailActivityTest.java
+++ ApkMirror/app/src/androidTest/java/com/pascalwelsch/apkmirror/detail/AppDetailActivityTest.java
@@ -2,7 +2,6 @@ package com.pascalwelsch.apkmirror.detail;
 
 import com.pascalwelsch.apkmirror.R;
 import com.pascalwelsch.apkmirror.model.AppUpdate;
-import com.squareup.picasso.Picasso;
 
 import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
@@ -25,13 +24,13 @@ public class AppDetailActivityTest extends ActivityInstrumentationTestCase2<AppD
 
     public void testAppIcon() throws Exception {
         final AppUpdate app = new AppUpdate();
-        app.setIconUrl("http://www.apkmirror.com/wp-content/uploads/2014/11/54655c2d1d190.png");
+        final String iconUrl
+                = "http://www.apkmirror.com/wp-content/uploads/2014/11/54655c2d1d190.png";
+        app.setIconUrl(iconUrl);
         final AppDetailActivity activity = startDetailActivity(app);
 
         final ImageView name = (ImageView) activity.findViewById(R.id.details_app_blurry_image);
         assertNotNull(name);
-        //https://engineering.aweber.com/unit-testing-remote-images-on-android-with-picasso-and-robolectric/
-        // TODO use MockPicasso
     }
 
     public void testAppName() throws Exception {
@@ -42,10 +41,6 @@ public class AppDetailActivityTest extends ActivityInstrumentationTestCase2<AppD
         final TextView name = (TextView) activity.findViewById(R.id.detail_app_name);
         assertNotNull(name);
         assertEquals(app.getName(), name.getText());
-    }
-
-    private void expectAppIcon(final AppDetailActivity activity) {
-
     }
 
     private AppDetailActivity startDetailActivity(final AppUpdate app) {
--- ApkMirror/app/src/main/java/com/pascalwelsch/apkmirror/detail/AppDetailFragment.java
+++ ApkMirror/app/src/main/java/com/pascalwelsch/apkmirror/detail/AppDetailFragment.java
@@ -1,22 +1,28 @@
 package com.pascalwelsch.apkmirror.detail;
 
+import com.nirhart.parallaxscroll.views.ParallaxScrollView;
 import com.pascalwelsch.apkmirror.R;
 import com.pascalwelsch.apkmirror.model.AppUpdate;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.RequestCreator;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
  * Created by pascalwelsch on 11/14/14.
  */
-public class AppDetailFragment extends Fragment {
+public class AppDetailFragment extends Fragment
+        implements ViewTreeObserver.OnScrollChangedListener {
 
     protected static final String INTENT_APP = "intent::app";
 
@@ -24,9 +30,20 @@ public class AppDetailFragment extends Fragment {
 
     protected static final String INTENT_TOUCH_Y = "intent::y_pos";
 
+    private static final String TAG = AppDetailFragment.class.getSimpleName();
+
     private AppUpdate mApp;
 
+    private ImageView mAppIcon;
+
     private ImageView mBlurryImage;
+
+    private int mHeaderHeight;
+
+    private ParallaxScrollView mParallaxContainer;
+
+    private Toolbar mToolbar;
+
 
     public static AppDetailFragment newInstance(final AppUpdate appUpdate, final int x,
             final int y) {
@@ -58,18 +75,47 @@ public class AppDetailFragment extends Fragment {
 
         final View rootView = inflater.inflate(R.layout.fragment_app_detail, null);
         mBlurryImage = (ImageView) rootView.findViewById(R.id.details_app_blurry_image);
+        mAppIcon = (ImageView) rootView.findViewById(R.id.details_app_icon);
         TextView appName = (TextView) rootView.findViewById(R.id.detail_app_name);
+        mToolbar = (Toolbar) rootView.findViewById(R.id.toolbar);
+        mParallaxContainer = (ParallaxScrollView) rootView
+                .findViewById(R.id.parallax_scrollview);
+        mParallaxContainer.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                ViewTreeObserver observer = mParallaxContainer.getViewTreeObserver();
+                observer.addOnScrollChangedListener(AppDetailFragment.this);
+                return false;
+            }
+        });
+        final View headerView = ((ViewGroup) mParallaxContainer.getChildAt(0)).getChildAt(0);
+        mHeaderHeight = headerView.getLayoutParams().height;
+        mToolbar.setTitle(mApp.getName());
         appName.setText(mApp.getName());
 
         return rootView;
     }
 
     @Override
+    public void onScrollChanged() {
+        int baseColor = getResources().getColor(R.color.apkmirror_primary);
+        float alpha = 1 - (float) Math.max(0, mHeaderHeight - mParallaxContainer.getScrollY()) / mHeaderHeight;
+        setBackgroundAlpha(mToolbar, alpha, baseColor);
+    }
+
+    private void setBackgroundAlpha(View view, float alpha, int baseColor) {
+        int a = Math.min(255, Math.max(0, (int) (alpha * 255))) << 24;
+        int rgb = 0x00ffffff & baseColor;
+        view.setBackgroundColor(a + rgb);
+    }
+
+    @Override
     public void onViewCreated(final View view, @Nullable final Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Picasso.with(getActivity())
-                .load(mApp.getIconUrl())
-                .transform(new BlurTransform(getActivity(), 10))
-                .into(mBlurryImage);
+        final RequestCreator requestCreator = Picasso.with(getActivity())
+                .load(mApp.getIconUrl());
+        requestCreator.into(mAppIcon);
+        requestCreator.transform(new BlurTransform(getActivity(), 8)).into(mBlurryImage);
     }
 }
